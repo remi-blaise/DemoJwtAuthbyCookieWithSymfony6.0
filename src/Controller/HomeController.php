1fbd0146e0332fc4e141bb26e66c0e25be0c93ff
Rémi Blaise <pro@remi-blaise.com>
2023-10-02 11:08:23 +0200

    Created The Authentication System: Users in Databse + Login + Logout + Registration

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('home/profile.html.twig', [
+        ]);
+    }
+}
